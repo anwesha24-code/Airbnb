4359da7e42e292ca049a6ef303cf74ca99fe1f82
Anwesha Pal <anweshapal2006@gmail.com>
2025-10-12 11:40:33 +0530

    custom marker added

--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,11 @@
 //they are only accessible in server-side code. To use the MAP_TOKEN in your client-side JavaScript, you need to pass it from your server to your client.
 //so we added a script tag in show.ejs to define a global variable mapToken with the value of process.env.MAP_TOKEN
 
+const el = document.createElement('div');
+el.className = 'marker';
+el.innerHTML='<i class="fa-regular fa-compass"></i>';//adding a compass icon to the marker
+
+
 mapboxgl.accessToken= mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
@@ -11,7 +16,7 @@ const map = new mapboxgl.Map({
 });
 console.log(listing.geometry.coordinates);
 // the the coordinates cannot be accessed directly since it is in public folder so we add coordinates in the top of show.ejs
-const marker=new mapboxgl.Marker({color:"red"})
+const marker=new mapboxgl.Marker(el)
 .setLngLat(listing.geometry.coordinates)//setting the marker at the coordinates of the listing as stored in the database
 .setPopup(
     new mapboxgl.Popup({offset:25})//adding a popup to the marker
